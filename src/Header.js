e765e60463510eaac4065cb47ec5dddbaba6b44d
tirlochan singh <sunny.trilochan@gmail.com>
2019-08-31 19:44:58 +0530

    initialised and header component added

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Header= () =>
+{
+    return(
+        <div className="header">
+            <div>Crypto Wiki</div>
+        </div>
+    )
+}
+export default Header;
