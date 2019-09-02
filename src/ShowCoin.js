fc53ee2fa17cfe8217825c66e350a5d579c33436
tirlochan singh <sunny.trilochan@gmail.com>
2019-09-02 10:57:25 +0530

    added panel to ShowCoin

--- src/ShowCoin.js
+++ src/ShowCoin.js
@@ -29,10 +29,32 @@ class ShowCoin extends Component {
                 <div>
                     <Form add={this.addcoin} />
                     <div className="panel-container">
-                    <div className="title">
-                    <a href={this.state.details.links.homepage[0]}><span>{this.state.details.name} {this.state.details.symbol}</span></a>
+                        <div className="panel-title">
+                            <a href={this.state.details.links.homepage[0]} target="blank"><span>{this.state.details.name}-{this.state.details.symbol}</span></a>
+                        </div>
+                        <div className="panel-body">
+                            <div className="left-panel">
+                                <img src={this.state.details.image.large} alt={this.state.details.name}></img>
+                                <div className="left-panel-description">
+                                    Country Of Origin : {this.state.details.country_origin}<br /><br />
+                                    Date of Appearance : {this.state.details.genesis_date}<br /><br />
+                                    Market Cap Rank : {this.state.details.market_cap_rank}<br /><br />
+                                    Coin Gecko-<br />
+                                    Rank : {this.state.details.coingecko_rank}<br />
+                                    Score : {this.state.details.coingecko_score}<br /><br />
+                                    Developer Score : {this.state.details.developer_score}<br /><br />
+                                    Community Score : {this.state.details.community_score}<br /><br />
+                                    Liquidity Score : {this.state.details.liquidity_score}<br /><br />
+                                    Public Interest Score : {this.state.details.public_interest_score}<br /><br />
+                                </div>
+                            </div>
+                            <div className="body-description">
+                                {this.state.details.description.en}
+                            </div>
+                        </div>
+
+
                     </div>
-                </div>
                 </div>
             ) :
             (
